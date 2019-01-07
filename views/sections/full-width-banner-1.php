a392d8d3cdd5bc4bb10bd247832ab17e48160c76
Kyle Knight <kyle@donedid.co.za>
2019-01-07 18:52:50 +0200

    Adjusted loading animation, using data-src instead of css class

--- views/sections/full-width-banner-1.php
+++ views/sections/full-width-banner-1.php
@@ -2,7 +2,7 @@
   <div class="banner banner--quote  flex flex--v-middle flex--h-middle flex--wrap">
     <div class="banner__bg lazyload" data-bg="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/banner-bg-1.jpg">
       <div class="inner-container">
-        <img class="banner__bg--overlay banner__bg--overlay--1 lazyload" data-src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/banner-bg-1-overlay.png" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" width="365" height="520" />
+        <img class="banner__bg--overlay banner__bg--overlay--1 lazyload" data-src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/banner-bg-1-overlay.png" src="data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJsb2FkZXItMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgeD0iMHB4IiB5PSIwcHgiIHdpZHRoPSI0MHB4IiBoZWlnaHQ9IjQwcHgiIHZpZXdCb3g9IjAgMCA1MCA1MCIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgNTAgNTA7IiB4bWw6c3BhY2U9InByZXNlcnZlIj4gIDxwYXRoIGZpbGw9IiMwMDAiIGQ9Ik00My45MzUsMjUuMTQ1YzAtMTAuMzE4LTguMzY0LTE4LjY4My0xOC42ODMtMTguNjgzYy0xMC4zMTgsMC0xOC42ODMsOC4zNjUtMTguNjgzLDE4LjY4M2g0LjA2OGMwLTguMDcxLDYuNTQzLTE0LjYxNSwxNC42MTUtMTQuNjE1YzguMDcyLDAsMTQuNjE1LDYuNTQzLDE0LjYxNSwxNC42MTVINDMuOTM1eiIgdHJhbnNmb3JtPSJyb3RhdGUoMjQxLjcxIDI1IDI1KSI+ICAgIDxhbmltYXRlVHJhbnNmb3JtIGF0dHJpYnV0ZVR5cGU9InhtbCIgYXR0cmlidXRlTmFtZT0idHJhbnNmb3JtIiB0eXBlPSJyb3RhdGUiIGZyb209IjAgMjUgMjUiIHRvPSIzNjAgMjUgMjUiIGR1cj0iMC42cyIgcmVwZWF0Q291bnQ9ImluZGVmaW5pdGUiPjwvYW5pbWF0ZVRyYW5zZm9ybT4gICAgPC9wYXRoPiAgPC9zdmc+" width="365" height="520" />
       </div>
     </div>
     <div class="outer-container">
