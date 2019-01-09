ad198b7a2242c6e1fee751130cdfb3754e3ccc28
Kyle Knight <kyle@donedid.co.za>
2019-01-09 20:13:43 +0200

    Updated Real Impact Copy

--- views/sections/real-impact.php
+++ views/sections/real-impact.php
@@ -7,7 +7,8 @@
           <div class="section__title section__title--pos-left">
             <h1 class="orientation--270">Real<br> Impact</h1>
           </div>
-          <div class="section__content section__content--intro">rewardStyle the Conference is an annual, invitation-only event for select global influeners and brands.</div>
+          <div class="section__content section__content--intro">rewardStyle the Conference is an annual, by-invitation event for high-performance global influencers and brands. Held in its hometown of Dallas, Texas, the conference started in 2012 and is celebrating its 7th season this year.
+</div>
           <div class="section__image">
             <div class="section__image__wrap loading">
               <img class="lazyload" data-src="<?php echo get_stylesheet_directory_uri(); ?>/dist/img/speaker-1.jpg" src="data:image/svg+xml;base64,PHN2ZyB2ZXJzaW9uPSIxLjEiIGlkPSJsb2FkZXItMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB4bWxuczp4bGluaz0iaHR0cDovL3d3dy53My5vcmcvMTk5OS94bGluayIgeD0iMHB4IiB5PSIwcHgiIHdpZHRoPSI0MHB4IiBoZWlnaHQ9IjQwcHgiIHZpZXdCb3g9IjAgMCA1MCA1MCIgc3R5bGU9ImVuYWJsZS1iYWNrZ3JvdW5kOm5ldyAwIDAgNTAgNTA7IiB4bWw6c3BhY2U9InByZXNlcnZlIj4gIDxwYXRoIGZpbGw9IiMwMDAiIGQ9Ik00My45MzUsMjUuMTQ1YzAtMTAuMzE4LTguMzY0LTE4LjY4My0xOC42ODMtMTguNjgzYy0xMC4zMTgsMC0xOC42ODMsOC4zNjUtMTguNjgzLDE4LjY4M2g0LjA2OGMwLTguMDcxLDYuNTQzLTE0LjYxNSwxNC42MTUtMTQuNjE1YzguMDcyLDAsMTQuNjE1LDYuNTQzLDE0LjYxNSwxNC42MTVINDMuOTM1eiIgdHJhbnNmb3JtPSJyb3RhdGUoMjQxLjcxIDI1IDI1KSI+ICAgIDxhbmltYXRlVHJhbnNmb3JtIGF0dHJpYnV0ZVR5cGU9InhtbCIgYXR0cmlidXRlTmFtZT0idHJhbnNmb3JtIiB0eXBlPSJyb3RhdGUiIGZyb209IjAgMjUgMjUiIHRvPSIzNjAgMjUgMjUiIGR1cj0iMC42cyIgcmVwZWF0Q291bnQ9ImluZGVmaW5pdGUiPjwvYW5pbWF0ZVRyYW5zZm9ybT4gICAgPC9wYXRoPiAgPC9zdmc+" />
@@ -17,7 +18,6 @@
 
         <div class="section__column">
           <div class="section__content">
-            <p>Held in its hometown of Dallas, Texas, the conference started in 2012 and is celebrating its 7th season this year.</p>
             <p>So, why come? Because the Conference is focused entirely on powering the hyper-growth of your business so you can make real impact. Not only will you be connected with global players in the influencer and retail spheres, but you’ll also be a part of  a full, three-day experience rich with educational sessions and one-of-a-kind networking opportunities.</p>
             <p>We all need a reset from time to time, so take advantage of this exclusive opportunity to reestablish your business strategy, form meaningful relationships with brand leaders and influencers from around the world, and gain insight from a team that is wholly committed to furthering the growth of your global business and turning your dreams into reality.</p>
             <p>Real influence. Real impact.</p>
